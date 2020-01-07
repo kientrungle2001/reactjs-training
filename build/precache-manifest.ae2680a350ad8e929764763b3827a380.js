e7a94e1ce7e3a5378c8a3ed6c50931ff8c03ffb4
Le Trung Kien <kienlt@smartosc.com>
2020-01-07 13:54:31 +0700

    init

---
+++ build/precache-manifest.ae2680a350ad8e929764763b3827a380.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8601f944a5ec7217547a81eb7b9b80b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d289326ca33dad4e4e36",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "de014559b8f1cc24b957",
+    "url": "/static/js/2.2889df50.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.2889df50.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d289326ca33dad4e4e36",
+    "url": "/static/js/main.1d4a0b61.chunk.js"
+  },
+  {
+    "revision": "8b17e31eab0b8b2b1853",
+    "url": "/static/js/runtime-main.a9c02851.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/components/App/Header.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class AppHeader extends Component {
+	render() {
+		return (
+			<header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+		)
+	}
+}
