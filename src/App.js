e0d39389bf4b566b7be05eb92223e1a9c09935f1
kienlt <kientrungle2001@gmail.com>
2020-01-08 22:50:19 +0700

    remove header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
-import AppHeader from 'components/App/Header';
 import CustomerAccountRegister from 'components/Customer/Account/Register';
 import CustomerAccountLogin from 'components/Customer/Account/Login';
 import LayoutTwoColumn from 'components/Layout/TwoColumn';
@@ -9,7 +8,6 @@ import LayoutTwoColumn from 'components/Layout/TwoColumn';
 function App() {
   return (
     <div>
-      <AppHeader />
       <LayoutTwoColumn right={<CustomerAccountRegister />} left={<CustomerAccountLogin />} />
     </div>
   );
